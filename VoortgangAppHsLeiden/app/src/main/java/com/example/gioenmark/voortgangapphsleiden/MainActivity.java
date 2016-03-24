671f37814a9b54f698bae761843b7b7f9d5c0e3d
Giovski <Giovski>
2016-03-24 11:56:32 +0100

    Invoegen van Het juiste icon voor de snackbar

--- VoortgangAppHsLeiden/app/src/main/java/com/example/gioenmark/voortgangapphsleiden/MainActivity.java
+++ VoortgangAppHsLeiden/app/src/main/java/com/example/gioenmark/voortgangapphsleiden/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "43 van de 102 punten behaald", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
